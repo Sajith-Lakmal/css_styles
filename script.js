e51d1760e0931694730986be407e1d180d67676f
Sajith-Lakmal <sajith.lakmal147@gmail.com>
2022-01-04 18:43:06 +0530

    error animation done

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.querySelector(".btn").addEventListener("click", () => {
-  document.querySelector(".btn").classList.toggle("button-animation");
+  document.querySelector(".btn").classList.add("button-animation");
   const time = setTimeout(() => {
     const btn = document.querySelector(".btn");
     btn.classList.remove("button-animation");
-    btn.classList.add("button-done-animation");
+    btn.classList.add("button-error-animation");
+    const finishTimer = setTimeout(() => {
+      btn.classList.remove("button-error-animation");
+    }, 4000);
   }, 4000);
 });
