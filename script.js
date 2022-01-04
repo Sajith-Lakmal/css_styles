ba7cf7d50f3c6ad558db7935af070ce2e60542b0
Sajith-Lakmal <sajith.lakmal147@gmail.com>
2022-01-04 19:08:33 +0530

    final commit - clean up

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ document.querySelector(".btn").addEventListener("click", () => {
   const time = setTimeout(() => {
     const btn = document.querySelector(".btn");
     btn.classList.remove("button-animation");
+    btn.disabled = true;
     btn.classList.add("button-error-animation");
     const finishTimer = setTimeout(() => {
       btn.classList.remove("button-error-animation");
+      btn.disabled = false;
     }, 4000);
   }, 4000);
 });
